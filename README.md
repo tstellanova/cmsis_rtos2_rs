d161757b6201dd51c29f66cf66c5ddfea2563e79
Todd Stellanova <tstellanova@users.noreply.github.com>
2020-02-08 14:42:04 -0800

    Add methods for timer start / stop, add some docs

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # cmsis_rtos2_rs
+
+A rust wrapper crate for the [CMSIS-RTOS2 API](http://www.keil.com/pack/doc/CMSIS_Dev/RTOS2/html/index.html), 
+which should be provided by another library,
+and linked into the consuming library or application.
+
+Typically the target architecture is provided by the build consuming this crate. 
+You can test this with eg:
+```
+cargo build --target thumbv7em-none-eabihf
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 #![deny(warnings)]
-#![deny(unsafe_code)]
 #![no_std]
 
 #[allow(non_upper_case_globals)]
@@ -11,4 +10,68 @@ pub mod cmsis_os2;
 #[allow(non_camel_case_types)]
 #[allow(non_snake_case)]
 pub mod os_tick;
+
+pub use cmsis_os2::*;
+
+pub fn rtos_kernel_initialize() -> cmsis_os2::osStatus_t {
+    unsafe {
+        cmsis_os2::osKernelInitialize()
+    }
+}
+
+pub fn rtos_kernel_start() -> cmsis_os2::osStatus_t {
+    unsafe {
+        cmsis_os2::osKernelStart()
+    }
+}
+
+pub fn rtos_kernel_get_tick_freq_hz() -> u32 {
+    unsafe {
+        cmsis_os2::osKernelGetTickFreq()
+    }
+}
+
+pub fn rtos_kernel_get_sys_timer_freq_hz() -> u32 {
+    unsafe {
+        cmsis_os2::osKernelGetSysTimerFreq()
+    }
+}
+
+pub fn rtos_os_thread_new(func: osThreadFunc_t,  argument: *mut cty::c_void,  attr: *const osThreadAttr_t) -> osThreadId_t {
+    unsafe {
+        cmsis_os2::osThreadNew(func, argument, attr)
+    }
+}
+
+pub fn rtos_os_thread_yield()  -> cmsis_os2::osStatus_t {
+    unsafe {
+        cmsis_os2::osThreadYield()
+    }
+}
+
+pub fn rtos_os_delay(ticks: u32) -> cmsis_os2::osStatus_t {
+    unsafe {
+        cmsis_os2::osDelay(ticks)
+    }
+}
+
+pub fn rtos_os_timer_new(func: osTimerFunc_t, timer_type: osTimerType_t,  argument: *mut cty::c_void,  attr: *const osTimerAttr_t) -> osTimerId_t {
+    unsafe {
+        cmsis_os2::osTimerNew(func, timer_type, argument, attr)
+    }
+}
+
+pub fn rtos_os_timer_start(timer_id: osTimerId_t, ticks: u32) -> osStatus_t {
+    unsafe {
+        cmsis_os2::osTimerStart(timer_id, ticks)
+    }
+}
+
+pub fn rtos_os_timer_stop(timer_id: osTimerId_t) -> osStatus_t {
+    unsafe {
+        cmsis_os2::osTimerStop(timer_id)
+    }
+}
+
+
 
